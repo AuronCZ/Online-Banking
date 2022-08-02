bb6d1dc820d3c8245e5f27e4910ef87e9bea1d0b
AuronCZ <ac51995@ubt-uni.net>
2022-08-02 18:37:46 +0200

    end3

---
+++ client-app/src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
+
+function App() {
+  const [accounts, setAccounts] = useState([]);
+
+  useEffect(() => {
+    axios.get('htpp://localhost:5000/api/accounts').then(response => {
+      console.log(response);
+      setAccounts(response.data);
+    })
+  }, [])
+
+  return (
+    <div>
+      <Header as='h2' icon='users' content='Banking System'/>
+      <header className="App-header">
+        <List>
+        {accounts.map((account: any) =>(
+            <List.Item key={account.id}>
+              {account.name}
+            </List.Item>
+          ))}
+        </List>
+      </header>
+    </div>
+  );
+}
+
+export default App;
